e540e2b85474a86f20e615bff64a6f1d6cbf6667
Zhen Ju <juzhenatpku@gmail.com>
2020-06-01 16:58:24 +0000

    Update ROSPATH export in setup_env.sh

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -4,7 +4,6 @@ echo $ROSPATH
 mkdir -p $ROSPATH/src
 mkdir -p $ROSPATH/install
 touch $ROSPATH/install/setup.bash
-echo 'ROSPATH=$HOME/ros/$ROS_DISTRO' >> $HOME/.bashrc
-
+echo 'export ROSPATH=$HOME/ros/$ROS_DISTRO' >> $HOME/.bashrc
 echo 'source $ROSPATH/install/setup.bash' >> $HOME/.bashrc
 
